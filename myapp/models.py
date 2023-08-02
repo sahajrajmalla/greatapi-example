51987ac0984b406233e1364d5d30e102a94a24a7
Sahaj Raj Malla <51092972+sahajrajmalla@users.noreply.github.com>
2023-08-02 07:49:46 +0545

    :sparkles: added relationship to User in Blog

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from greatapi.db.admin.user import User
 from greatapi.db.database import Base
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class Blog(Base):
     __tablename__ = "blogs"
-    
+
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey(User.id))
+
+    user = relationship(User)
--- myapp/schemas.py
+++ myapp/schemas.py
@@ -3,12 +3,15 @@ from pydantic import BaseModel
 class BlogBase(BaseModel):
     title: str
     body: str
+    user_id: int  # Add the user_id field to represent the foreign key relationship
+
 
 class BlogCreate(BlogBase):
     pass
 
 class BlogSchema(BlogBase):
     id: int
+
 
     class Config:
         orm_mode = True
